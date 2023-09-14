5fdcba0beea463d5e0d0cb8d78698931c9e4a19e
cjvrd <cjvirdo@gmail.com>
2023-09-14 16:47:32 +1000

    training programs javascript object files
    converting raw training programs into javascript objects to make them accessible for dynamic population on webpage

---
+++ training programs/programobjects/middlebuildstrength.js
@@ -0,0 +1,120 @@
+const middleBuildStrength = {
+  weeks: [
+    {
+      weekNumber: 1,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 3, reps: 5 },
+            { name: "Hamstring Curl", sets: 2, reps: 10 },
+            { name: "Leg Extension", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 5 },
+            { name: "Dip", sets: 2, reps: 10 },
+            { name: "Row", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 5 },
+            { name: "Bench Press", sets: 2, reps: 5 },
+            { name: "Deadlift", sets: 2, reps: 5 },
+          ],
+        },
+      ],
+    },
+    {
+      weekNumber: 2,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 3, reps: 3 },
+            { name: "Hamstring Curl", sets: 2, reps: 10 },
+            { name: "Leg Extension", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 3 },
+            { name: "Dip", sets: 2, reps: 10 },
+            { name: "Row", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 3 },
+            { name: "Bench Press", sets: 2, reps: 3 },
+            { name: "Deadlift", sets: 2, reps: 3 },
+          ],
+        },
+      ],
+    },
+    {
+      weekNumber: 3,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 3, reps: 2 },
+            { name: "Hamstring Curl", sets: 2, reps: 10 },
+            { name: "Leg Extension", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 2 },
+            { name: "Dip", sets: 2, reps: 10 },
+            { name: "Row", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 2 },
+            { name: "Bench Press", sets: 2, reps: 2 },
+            { name: "Deadlift", sets: 2, reps: 2 },
+          ],
+        },
+      ],
+    },
+    {
+      weekNumber: 4,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 3, reps: 1 },
+            { name: "Hamstring Curl", sets: 2, reps: 10 },
+            { name: "Leg Extension", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 3, reps: 1 },
+            { name: "Dip", sets: 2, reps: 10 },
+            { name: "Row", sets: 2, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 1 },
+            { name: "Bench Press", sets: 2, reps: 1 },
+            { name: "Deadlift", sets: 2, reps: 1 },
+          ],
+        },
+      ],
+    },
+  ],
+};
---
+++ training programs/programobjects/oldbuildstrength.js
@@ -0,0 +1,120 @@
+const oldBuildStrength = {
+  weeks: [
+    {
+      weekNumber: 1,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 5 },
+            { name: "Hamstring Curl", sets: 1, reps: 10 },
+            { name: "Leg Extension", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 2, reps: 5 },
+            { name: "Tricep Extension", sets: 1, reps: 10 },
+            { name: "Lat Pulldown", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 1, reps: 5 },
+            { name: "Bench Press", sets: 1, reps: 5 },
+            { name: "Deadlift", sets: 1, reps: 5 },
+          ],
+        },
+      ],
+    },
+    {
+      weekNumber: 2,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 3 },
+            { name: "Hamstring Curl", sets: 1, reps: 10 },
+            { name: "Leg Extension", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 2, reps: 3 },
+            { name: "Tricep Extension", sets: 1, reps: 10 },
+            { name: "Lat Pulldown", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 3 },
+            { name: "Bench Press", sets: 1, reps: 3 },
+            { name: "Deadlift", sets: 1, reps: 3 },
+          ],
+        },
+      ],
+    },
+    {
+      weekNumber: 3,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 2 },
+            { name: "Hamstring Curl", sets: 1, reps: 10 },
+            { name: "Leg Extension", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 2, reps: 2 },
+            { name: "Tricep Extension", sets: 1, reps: 10 },
+            { name: "Lat Pulldown", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 1, reps: 2 },
+            { name: "Bench Press", sets: 1, reps: 2 },
+            { name: "Deadlift", sets: 1, reps: 2 },
+          ],
+        },
+      ],
+    },
+    {
+      weekNumber: 4,
+      days: [
+        {
+          dayNumber: 1,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 1 },
+            { name: "Hamstring Curl", sets: 1, reps: 10 },
+            { name: "Leg Extension", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 2,
+          exercises: [
+            { name: "Bench Press", sets: 2, reps: 1 },
+            { name: "Tricep Extension", sets: 1, reps: 10 },
+            { name: "Lat Pulldown", sets: 1, reps: 10 },
+          ],
+        },
+        {
+          dayNumber: 3,
+          exercises: [
+            { name: "Squat", sets: 2, reps: 1 },
+            { name: "Bench Press", sets: 1, reps: 1 },
+            { name: "Deadlift", sets: 1, reps: 1 },
+          ],
+        },
+      ],
+    },
+  ],
+};
