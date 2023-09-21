5f4095277c3079b7823d650826978d09d44b69b9
Abdulmajeed <majeed.garoot@gmail.com>
2023-09-21 13:35:51 +1000

    updated cycle.js

--- models/cycle.js
+++ models/cycle.js
@@ -1,26 +1,15 @@
-
 const trainingPrograms = require('../training-programs/program-objects');
 let client = require('../dbConnection');
 let collection = client.db().collection('Cycle');
 
-function createCycleForUser(user, callback){
-    let age = "";
-    // console.log(trainingPrograms.oldBuildStrength)
-    let userAge = parseInt(user.age);
-    if (userAge > 65){
-        age = "old"
-    } else if (userAge > 39 && userAge <= 65){
-        age = "middle"
-    } else {
-        age = "young"
-    }
+function createCycleForUser(user, callback) {
 
     let newCycle = {
         userId: user._id,
-        program: pickProgram(age, user.goal),
+        program: pickProgram(user.age, user.goal), //changed to user.age
     };
-    collection.insertOne(newCycle, (err, result) =>{
-        if(err) {
+    collection.insertOne(newCycle, (err, result) => {
+        if (err) {
             return callback(err);
         }
 
@@ -29,46 +18,43 @@ function createCycleForUser(user, callback){
     });
 };
 
-function pickProgram(age, goal){
+function pickProgram(age, goal) { //this function takes in form data and assigns a program according to selection of age+goal
     // for old users
-    if (age === "old"){
-        if(goal === "Strength Training"){
+    if (age === "old") {
+        if (goal === "buildStrength") {
             return trainingPrograms.oldBuildStrength;
-        } else if (goal === "Weight Loss"){
+        } else if (goal === "loseWeight") {
             return trainingPrograms.oldLoseWeight;
-        } else if (goal === "Build Mass"){
+        } else if (goal === "buildMuscle") {
             return trainingPrograms.oldBuildMuscle;
         };
-    }
+    };
     // for middle-age users
-    if (age === "middle"){
-        if(goal === "Strength Training"){
+    if (age === "middle") {
+        if (goal === "buildStrength") {
             return trainingPrograms.middleBuildStrength;
-        } else if (goal === "Weight Loss"){
+        } else if (goal === "loseWeight") {
             return trainingPrograms.middleLoseWeight;
-        } else if (goal === "Build Mass"){
+        } else if (goal === "buildMuscle") {
             return trainingPrograms.middleBuildMuscle;
         };
-    }
+    };
     // for young users
-    if (age === "young"){
-        if(goal === "Strength Training"){
+    if (age === "young") {
+        if (goal === "buildStrength") {
             return trainingPrograms.youngBuildStrength;
-        } else if (goal === "Weight Loss"){
+        } else if (goal === "loseWeight") {
             return trainingPrograms.youngLoseWeight;
-        } else if (goal === "Build Mass"){
+        } else if (goal === "buildMuscle") {
             return trainingPrograms.youngBuildMuscle;
         };
-    }
+    };
 
-    return null
+    return null;
 }
 
-function getCyclesForUser(userId, callback){
+function getCyclesForUser(userId, callback) {
     collection.find({ userId: userId }).toArray(callback);
 }
 
-module.exports = {
-    createCycleForUser,
-    getCyclesForUser,
-};
+module.exports = {createCycleForUser, getCyclesForUser};
