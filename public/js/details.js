d029f5274d6dd12b38caba2423242b97f212f7d4
Abdulmajeed <majeed.garoot@gmail.com>
2023-09-24 23:07:49 +1000

    + details.html + details.css + details.js

---
+++ public/js/details.js
@@ -0,0 +1,4 @@
+// function to get first week after the last week that's labeled 'done=true' last
+    // store week number
+// function to get first day after the last day that's labeled 'done=true' last
+
