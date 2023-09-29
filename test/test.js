cde2c1439f8f27f24c43228fdc372042100671f1
cjvrd <cjvirdo@gmail.com>
2023-09-29 15:43:13 +1000

    updated test.js
    - created a random function that creates a random email string to be used for each test, if it uses a static string, the test fails if run a second time
    
    - modified all test to have try/catch error handling, otherwise they always pass
    
    - removed incorrect error handling from sign up test
    
    - added a new unit test for checking unsuccessful sign up

--- test/test.js
+++ test/test.js
@@ -3,9 +3,13 @@ const axios = require("axios");
 
 const baseUrl = 'http://localhost:3000';
 
+const randomEmail = (Math.random().toString(16).substr(2, 8)) + '@example.com'; //creates a random email for new user each time test is run
+
+console.log(randomEmail);
+
 const signUpData = {
     fullName: 'John Doe',
-    email: 'john@example2.com',
+    email: randomEmail,
     password: 'password123',
     age: 'middle',
     goal: 'buildStrength',
@@ -13,23 +17,28 @@ const signUpData = {
 };
 
 const signInData = {
-    email: 'john@example.com',
+    email: randomEmail,
     password: 'password123'
 };
 
 describe('GET /users API', function() {
     it('should return a status code of 200 and an array of users', async function() {
-        const response = await axios.get(`${baseUrl}/users`);
-        expect(response.status).to.equal(200);
-        expect(response.data).to.be.an('object');
-        expect(response.data.statusCode).to.equal(200);
-        expect(response.data.message).to.equal('Retrieved all users successfully');
-        expect(response.data.data).to.be.an('array');
-        //console.log(response.data);
+        try {
+            // Make a GET request to the users endpoint
+            const response = await axios.get(`${baseUrl}/users`);
+            // check the response status and data
+            expect(response.status).to.equal(200);
+            expect(response.data).to.be.an('object');
+            expect(response.data.statusCode).to.equal(200);
+            expect(response.data.message).to.equal('Retrieved all users successfully');
+            expect(response.data.data).to.be.an('array');
+        } catch (error) {
+            throw new Error(`${error.message}`);    
+        }
     });
 });
 
-describe('Test SignUp API', function() {
+describe('Test SignUp API success', function() {
     it('Signs up a user and returns a status code of 201', async function() {
         try {
             // Make a POST request to the signup endpoint
@@ -41,33 +50,37 @@ describe('Test SignUp API', function() {
             // Check the response message for successful registration
             expect(response.data.message).to.equal('User registered successfully');
         } catch (error) {
-            // Handle any errors or unexpected status codes
-            if (error.response) {
-                if (error.response.status === 400) {
-                    // message for email already in use
-                    expect(error.response.data.message).to.equal('Email already in use');
-                } else if (error.response.status === 500) {
-                    // message for server error during signup
-                    expect(error.response.data.message).to.equal('Server error when checking email');
-                } else {
-                    // Unexpected status code
-                    throw new Error(`Unexpected status code: ${error.response.status}`);
-                }
-            } else {
-                // Handle other errors (e.g., network error)
-                throw new Error(`Error: ${error.message}`);
-            }
+            throw new Error(`${error.message}`);
         }
     });
 });
 
+// this test should pass as its done after the sign up test, which creates the test user
+describe('Test SignUp API unsuccessful', function() {
+    it('Checks email that already exists and returns a status code of 400', async function() {
+        try {
+            // Make a POST request to the signup endpoint
+            const response = await axios.post(`${baseUrl}/signup`, signUpData);
+
+            // Check the response status code
+            expect(response.data.statusCode).to.equal(400);
+
+            // Check the response message for unsuccessful registration
+            expect(response.data.message).to.equal('Email already in use');
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
+    })
+})
+
 describe('Test SignIn API', function() {
     it('Signs in a user and returns a status code of 200', async function() {
-        const response = await axios.post(`${baseUrl}/signin`, signInData);
-        expect(response.status).to.equal(200);
-        expect(response.data.message).to.equal('Logged in successfully');
+        try {
+            const response = await axios.post(`${baseUrl}/signin`, signInData);
+            expect(response.status).to.equal(200);
+            expect(response.data.message).to.equal('Logged in successfully');
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
     });
 });
-
-
-
