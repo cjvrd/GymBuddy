1b3154b6422a328b972fada492e7727ef30f3836
Abdulmajeed <majeed.garoot@gmail.com>
2023-09-30 23:25:32 +1000

    + week1.html automatically opens current day and current week

--- public/js/displayprogram.js
+++ public/js/displayprogram.js
@@ -56,3 +56,44 @@ if (window.location.href.endsWith('/week4.html')) {
     displayDay(cycles[0].program.weeks[3].days[1], 'dayTwoBody');
     displayDay(cycles[0].program.weeks[3].days[2], 'dayThreeBody');
 };
+
+function toggleCollapse(dayNumber){
+    var button1 = document.querySelector('#day1');
+    var instance1 = new bootstrap.Collapse(button1, { toggle: false }); // ensure toggle is set to false
+    var button2 = document.querySelector('#day2');
+    var instance2 = new bootstrap.Collapse(button2, { toggle: false }); // ensure toggle is set to false
+    var button3 = document.querySelector('#day3');
+    var instance3 = new bootstrap.Collapse(button3, { toggle: false }); // ensure toggle is set to false
+
+    if(dayNumber === 1){
+        instance1.show();
+    } else if (dayNumber === 2){
+        instance2.show();
+    } else if (dayNumber === 3){
+        instance3.show();
+    }
+}
+
+function setActiveWeek(weekNumber) {
+    // select all navigation links
+    var navLinks = document.querySelectorAll('.nav-link');
+    for (var i =0; i < navLinks.length; i++){
+        if(i === weekNumber - 1){
+            navLinks[i].classList.add('active');
+        } else {
+            navLinks[i].classList.remove('active');
+        }
+    }
+}
+
+$(document).ready(function () {
+    var cycles = JSON.parse(localStorage.getItem('userCycles'));
+    var currentWeekDay = parseInt(localStorage.getItem('currentWeekDay'));
+    var currentWeekNumber = parseInt(localStorage.getItem('currentWeekNumber'));
+    console.log(cycles[0].program);
+
+    // open current day
+    toggleCollapse(currentWeekDay);
+    setActiveWeek(currentWeekNumber);
+
+});
